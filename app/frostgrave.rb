a647eef3bfd21a5c5b03601bb757cf6ad5ea91c7
Cédric ZUGER <cedric.zuger@rdsdiag.com>
2021-04-24 12:41:54 +0200

    Choose spells for school limited to 3.

--- app/frostgrave.rb
+++ app/frostgrave.rb
@@ -4,6 +4,10 @@ get '/' do
   haml :index
 end
 
-get '/create' do
+get '/new_mage_create' do
   haml :new_mage_create
+end
+
+get '/new_mage_school_spells' do
+  haml :new_mage_school_spells, :locals => {mage_type: params[:mage_type]}
 end
---
+++ app/public/js/new_mage_school_spells.js
@@ -0,0 +1,49 @@
+/**
+ * Created by cedric on 24/04/2021.
+ */
+
+var selected_components = new Set([]);
+
+Vue.component('spell-button', {
+    data: function () {
+        return { selected: false, nonSelected: 'btn-light', Selected: 'btn-primary', Btn: 'btn'}
+    },
+    props: ['spell_name'],
+    template: '<button type="button" :class="[selected ? Selected : nonSelected, Btn]"  @click="clicked">{{ spell_name }}</button>',
+    methods: {
+        // whenever question changes, this function will run
+        clicked(event) {
+            if(this.selected) {
+                this.selected = false;
+                selected_components.delete(this.spell_name);
+            }
+            else{
+                if(selected_components.size <3){
+                    this.selected = true;
+                    selected_components.add(this.spell_name);
+                }
+            }
+
+            console.log(selected_components);
+        }
+    }
+});
+
+
+// L'objet est ajouté à une instance de Vue
+new Vue({
+    el: '#wizard-choose-spells',
+    data: { spells: [] },
+    mounted: function () {
+        this.name = name_gen();
+
+        axios
+            .get('/spells.json')
+            .then(response => {
+                const result = response.data;
+                const mage_type = $('#mage_type').val();
+
+                this.spells = result[mage_type]['spells'].map(x => x['title']);
+    });
+}});
+
