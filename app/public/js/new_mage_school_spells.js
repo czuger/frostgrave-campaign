e0a8c5a77d48ca3295f72ad149079901a97326ab
Cédric ZUGER <cedric.zuger@rdsdiag.com>
2021-04-24 13:24:35 +0200

    Add popover for spell information.

--- app/public/js/new_mage_school_spells.js
+++ app/public/js/new_mage_school_spells.js
@@ -3,15 +3,25 @@
  */
 
 var selected_components = new Set([]);
+var spells_items = {};
 
 Vue.component('spell-button', {
     data: function () {
         return { selected: false, nonSelected: 'btn-light', Selected: 'btn-primary', Btn: 'btn btn-block'}
     },
-    props: ['spell_name'],
-    template: '<button type="button" :class="[selected ? Selected : nonSelected, Btn]"  @click="clicked">{{ spell_name }}</button>',
+    props: ['spell_name', 'spell_content'],
+    template: `
+        <div class="row mt-2">
+        <div class="col-9">
+            <button type="button" :class="[selected ? Selected : nonSelected, Btn]" @click="select">{{ spell_name }}</button>        
+        </div>
+        <div class="col-3">
+            <button type="button" class="btn btn-block btn-warning" data-toggle="popover" data-placement="left" :data-content="spell_content">Info</button>        
+        </div>
+        </div>
+    `,
     methods: {
-        clicked(event) {
+        select(event) {
             if(this.selected) {
                 this.selected = false;
                 selected_components.delete(this.spell_name);
@@ -42,7 +52,16 @@ new Vue({
                 const result = response.data;
                 const mage_type = $('#mage_type').val();
 
-                this.spells = result[mage_type]['spells'].map(x => x['title']);
+                for (const spell of result[mage_type]['spells']) {
+                    this.spells.push(spell);
+                    spells_items[spell['title']]=spell;
+                }
+
+        this.$nextTick(() => {
+            // Fires before full page is rendered
+            $('[data-toggle="popover"]').popover({
+                trigger: 'focus'
+            });
+        });
     });
 }});
-
